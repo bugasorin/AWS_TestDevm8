f08b0c1734cec8d7a391c135fd158905ecb371c8
Sorin <bugasorin@gmail.com>
2023-01-19 22:05:43 +0200

    added another files

---
+++ 2-eks.tf
@@ -0,0 +1,56 @@
+module "eks" {
+  source = "terraform-aws-modules/eks/aws"
+
+  cluster_name = "Devm8-eks"
+
+
+  cluster_endpoint_private_access = true
+  cluster_endpoint_public_access  = true
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  enable_irsa = true
+
+  eks_managed_node_group_defaults = {
+    disk_size = 50
+  }
+
+  eks_managed_node_groups = {
+    general = {
+      desired_size = 1
+      min_size     = 1
+      max_size     = 10
+
+      labels = {
+        role = "general"
+      }
+
+      instance_types = ["t3.micro"]
+      capacity_type  = "ON_DEMAND"
+    }
+
+    spot = {
+      desired_size = 1
+      min_size     = 1
+      max_size     = 10
+
+      labels = {
+        role = "spot"
+      }
+
+      taints = [{
+        key    = "market"
+        value  = "spot"
+        effect = "NO_SCHEDULE"
+      }]
+
+      instance_types = ["t3.micro"]
+      capacity_type  = "SPOT"
+    }
+  }
+
+  tags = {
+    Environment = "staging"
+  }
+}
